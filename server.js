6cdbdf2d0083796cbe888087feb51d535f5e1ef6
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2022-12-26 22:06:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,99 +19,25 @@ var MongoDBStore = require('connect-mongodb-session')(session);
 var vinyl = require("./models/vinyl");
 var User = require("./models/user");
 var Cart = require("./models/cart");
-var seedvinyl = require("./seed/vinyl");
-var seedWall = require('./seed/wallpaper');
-var seedsatin = require('./seed/satin');
-var seedkitchen = require('./seed/kitchen');
-var seedgliter = require('./seed/gliter');
-var seedspray = require('./seed/spray');
-var seeddeco = require('./seed/deco');
-var seedfloor = require('./seed/floor');
+
+
 var seedwow = require('./seed/wow');
-var seedtool = require('./seed/tool');
-var seedfitnes = require('./seed/fitnes');
-var seedtravel = require('./seed/travel');
-var seedscooter = require('./seed/scooter');
 var seedfurniteur = require('./seed/furniteur');
-var seedelectrical = require('./seed/electrical');
 var seedrug = require('./seed/rug');
 var seedcuisin = require('./seed/cuisin');
-var seeddecor = require('./seed/decor');
-var seedcurtain = require('./seed/curtain');
-var seedbedding = require('./seed/bedding');
-var seedsoft = require('./seed/soft');
-var seedstation = require('./seed/station');
 var seedclean = require('./seed/clean');
-var seedhome = require('./seed/home');
-var seedhome1 = require('./seed/home1');
-var seedpaint = require('./seed/paintH');
-var seedsport = require('./seed/sportH');
 var seedsale = require('./seed/saleH');
-var seeddeal = require('./seed/dealH');
-var seedinterior = require('./seed/interior');
 var seedheader = require('./seed/header');
-var seedblander = require('./seed/eblander');
-var seedcleaning = require('./seed/ecleaning');
-var seedcoffee = require('./seed/ecoffee');
-var seedfood = require('./seed/efood');
-var seedgaming = require('./seed/egaming');
-var seedgarden = require('./seed/egarden');
-var seedhealth = require('./seed/ehealth');
-var seediron = require('./seed/eiron');
-var seedmicro = require('./seed/emicro');
-var seedmobile = require('./seed/emobile');
-var seedvacuum = require('./seed/evacuum');
 var seedshipping = require('./seed/shipping');
-var seedpower = require('./seed/power');
-var seedhand = require('./seed/hand');
-var seedladder= require('./seed/ladder');
 
 
-
-var Wall = require('./models/wallpaper');
-var satin = require('./models/satin');
-var kitchen = require('./models/kitchen');
-var gliter = require('./models/gliter');
-var spray = require('./models/spray');
-var deco = require('./models/deco');
-var floor = require('./models/floor');
 var wow = require('./models/wow');
-var tool = require('./models/tool');
-var fitnes = require('./models/fitnes');
-var scooter = require('./models/scooter');
-var travel = require('./models/travel');
 var furniteur = require('./models/furniteur');
-var electrical = require('./models/electrical');
 var rug = require('./models/rug');
 var cuisin = require('./models/cuisin');
-var decor = require('./models/decor');
-var curtain = require('./models/curtain');
-var bedding = require('./models/bedding');
-var soft = require('./models/soft');
-var station = require('./models/station');
 var clean = require('./models/clean');
-var home = require('./models/home');
-var home1 = require('./models/home1');
-var paint = require('./models/paintH');
-var sport = require('./models/sportH');
 var sale = require('./models/saleH');
-var deal = require('./models/dealH');
-var interior = require('./models/interior');
 var header = require('./models/header');
-var blander = require('./models/blander');
-var cleaning = require('./models/cleaning');
-var coffee = require('./models/coffee');
-var food = require('./models/food');
-var gaming = require('./models/gaming');
-var garden = require('./models/garden');
-var health = require('./models/health');
-var iron = require('./models/iron');
-var micro = require('./models/micro');
-var mobile = require('./models/mobile');
-var vacuum = require('./models/vacuum');
-var power = require('./models/power');
-var ladder = require('./models/ladder');
-var hand = require('./models/hand');
 var shipping = require('./models/shipping');
 
 
@@ -183,12 +109,8 @@ app.use('/scss',express.static(__dirname + '/scss'))
 app.use('/roboto',express.static(__dirname + 'font/roboto'))
 app.use(express.static('node_modules'))
 app.use(flash());
-seedvinyl(); seedWall(); seedsatin(); seedkitchen(); seedgliter(); seedspray(); seeddeco(); seedfloor(); seedwow();
-seedtool(); seedscooter(); seedfitnes(); seedtravel(); seedfurniteur(); seedelectrical(); seedrug();
-seedcuisin(); seeddecor(); seedsoft(); seedbedding(); seedcurtain(); seedstation(); seedclean(); seedhome();
-seedhome1(); seedpaint(); seedsport(); seedsale(); seeddeal(); seedinterior(); seedblander(); seedcleaning(); seedcoffee();seedfood();seedgaming();seedgarden();seedhealth();seediron();seedmicro();seedmobile();seedvacuum();
-seedheader(); seedhand(); seedpower(); seedladder(); 
-seedshipping(); 
+ seedwow(); seedfurniteur();  seedrug(); seedcuisin();  seedclean(); seedsaleH();
+seedheader();  seedshipping(); 
 
 //bring events routes
 var routes = require('./routes/index');
