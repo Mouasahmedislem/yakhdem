d2a1822028813c136f171da10049c3018a84a8a6
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2022-12-27 23:35:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.use(function(req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
     // render the error page
     res.status(err.status || 500);
     res.render('error');
