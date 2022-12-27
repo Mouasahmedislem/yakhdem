c6fddcd2cf889c654972134cd59c4600f6d3d955
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2022-12-27 23:14:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.use('/css',express.static(__dirname + '/css'))
 app.use('/js',express.static(__dirname + '/js'))
 app.use('/img',express.static(__dirname + '/img'))
 app.use('/scss',express.static(__dirname + '/scss'))
-app.use('/roboto',express.static(__dirname + 'font/roboto'))
+app.use('/roboto',express.static(__dirname + '/font/roboto'))
 app.use(express.static('node_modules'))
 app.use(flash());
  seedwow(); seedfurniteur();  seedrug(); seedcuisin();  seedclean(); seedsale();
