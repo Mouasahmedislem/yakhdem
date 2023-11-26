f1c1fdbf9bc7ace0360746a56660dddb2c8c278d
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2023-11-26 21:31:12 +0100

    Update saleH.js

--- seed/saleH.js
+++ seed/saleH.js
@@ -21,23 +21,23 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
 },
 {
     image: "https://s8.gifyu.com/images/IMG-20220413-WA0027.jpg",
-    title: "Exterior Paint",
+    title: "Vinyl Paint",
     href:"/sale/furniteur"
 
 },
 {
     image: "https://s7.gifyu.com/images/IMG-20220413-WA003403cc89faebbf9981.jpg",
-    title: "Cuisine PAINT",
+    title: "Cuisine Paint",
     href:"/sale/rug"
 },
 {
     image: "https://s7.gifyu.com/images/IMG-20220413-WA0011.jpg",
-    title: "Bathroom PAINT",
+    title: "Bathroom Paint",
     href:"/sale/rug"
 },
 {
     image: "https://cdn.bmstores.co.uk/images/dmCategory/thumbnailImageResponsive/decorating-tools1.jpg",
-    title: "outils paint",
+    title: "Outils paint",
     href:"/sale/cuisin"
 }
 
