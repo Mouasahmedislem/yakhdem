c9fea9926ce866393ef9502a57d7de2920611679
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2023-11-30 23:42:05 +0100

    Update cuisin.js

--- seed/cuisin.js
+++ seed/cuisin.js
@@ -13,41 +13,81 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
 
 var cuisins = [
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/02/0804/1.jpg?3886",
-        title: "Pulvérisateur D'Huile En Verre Vaporisateur - Rechargeable",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/platoir-decorative-inox-trapeze-768x512.jpg",
+        title: "Platoir inox ",
         price: 350
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/23/1772/1.jpg?6140",
-        title: "Marteau À Viande Milanais En Bois",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/Couteau-pate-enduit%E2%80%8B.jpg",
+        title: "Couteau à Enduire Lame Arrondie",
         price: 500
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/91/62/1.jpg?7630",
-        title: "Corbeille A Fruits/Planche À Découper (Fruit Basket) -Marron",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/couteau-enduire-droite-768x512.jpg",
+        title: "Couteau à Enduire Lame Droite",
         price: 920
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/95/9393/4.jpg?6087",
-        title: "Organisateur De Cuisine Magnétique Porte Papier,Épices,Serviettes Multifonction ,Organisateur Réfrigérateur-Noir",
+        image: "https:https://rolux.dz/wp-content/uploads/2022/02/ruban-adhesif-masquage-768x511.jpg",
+        title: "Papier Adhésif De Masquage",
         price: 2350
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/97/8703/1.jpg?7085",
-        title: "Bestway Sac Glacière Isotherme - 15 Litres - Vert",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/spalter-768x512.jpg",
+        title: "Spalter",
         price: 3420
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/10/5104/1.jpg?4728",
-        title: "MONTELLA Marmite - Vivo - Pot En Granit - Couvercle En Verre",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/brosse-peinture-plate-768x512.jpg",
+        title: "Brosse Plate",
         price: 3750
     },
     {
-        image: "https://dz.jumia.is/unsafe/fit-in/500x500/filters:fill(whie)/product/06/0822/1.jpg?5384",
-        title: "Poubelle Suspendue Pliable Pour Cuisine Voiture",
+        image: "https://rolux.dz/wp-content/uploads/2022/02/pinceau-plat-bleu-peinture-768x512.jpg",
+        title: "Pinceau Plat Manche Bleu",
+        price: 1550
+    },
+    {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/pinceau-rond-peinture-768x512.jpg",
+        title: "Pinceau Rond Manche Bi-Matiére",
+        price: 1550
+    },
+  {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/rouleaux-decorative-nid-dabeille-768x512.jpg",
+        title: "Rouleaux Nid D'abeille 200 mm / 80 Ø",
+        price: 1550
+    },
+  {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/recharge-mini-rouleau-peinture-eponge-768x512.jpg",
+        title: "Recharge Mini Rouleau Eponge",
+        price: 1550
+    },
+  {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/recharge-mini-rouleau-peinture-polyacrylique-768x512.jpg",
+        title: "Recharge Mini Rouleau PolyAcrylique",
+        price: 1550
+    },
+  {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/monture-mini-rouleau-peinture-768x512.jpg",
+        title: "Monture Mini Rouleau ",
+        price: 1550
+    },
+   {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/mini-rouleau-peinture-microfibre-768x512.jpg",
+        title: "Mini Rouleau Microfibre Antigoutte 120 mm / 25 Ø",
+        price: 1550
+    },
+   {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/Rouleau-peinture-acrylique-Facade-768x513.jpg",
+        title: "Rouleau Acrylique Façade 200 mm / 44 Ø",
+        price: 1550
+    },
+   {
+        image: "https://rolux.dz/wp-content/uploads/2022/02/Rouleau-peinture-microfibre--768x513.jpg",
+        title: "Rouleau Microfibre 200 mm / 44 Ø",
         price: 1550
     }
-    
+  
  
 ];
 
