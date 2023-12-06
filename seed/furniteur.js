4d0357e69540556f3eaf009af71e0b21b26d07ff
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2023-12-06 23:42:51 +0100

    Update furniteur.js

--- seed/furniteur.js
+++ seed/furniteur.js
@@ -15,33 +15,33 @@ var furniteurs = [
     {
         image: "https://s5.gifyu.com/images/SRVVH.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Brilliant White 4KG",
-        price: 800.00
+        price: 900.00
     },
    
     {
         image: "https://s5.gifyu.com/images/SRVVx.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Red Spice 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVVs.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Mocha 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVVL.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Deep Amethyst 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVVp.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Dark Angel 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVVz.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Burnt Sugar 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVVb.jpg",
@@ -51,17 +51,17 @@ var furniteurs = [
     {
         image: "https://s5.gifyu.com/images/SRVVS.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Rich Red 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVV2.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Sweet Lavender 4KG",
-        price: 900.00
+        price: 1100.00
     },
     {
         image: "https://s5.gifyu.com/images/SRVUe.jpg",
         title: "Paintello Paint Vinyl Silk Emulsion - Lime Crush 4KG",
-        price: 900.00
+        price: 1100.00
     }
  
 ];
