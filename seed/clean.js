513a3902facadd0e53664ef0c95b754a129c15de
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2023-12-06 23:41:01 +0100

    Update clean.js

--- seed/clean.js
+++ seed/clean.js
@@ -21,57 +21,57 @@ var cleans = [
     {
         image: "https://s5.gifyu.com/images/SRFTR.jpg",
         title: "'Paintello's Paint Washable Satinée - Red Spice 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFT8.jpg",
         title: "'Paintello's Paint Washable Satinée - Mocha 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFT6.jpg",
         title: "'Paintello's Paint Washable Satinée - Deep Amethyst 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFT4.jpg",
         title: "'Paintello's Paint Washable Satinée - Dark Angel 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTg.jpg",
         title: "'Paintello's Paint Washable Satinée - Burnt Sugar 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTc.jpg",
         title: "'Paintello's Paint Washable Satinée - Aqua 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTu.jpg",
         title: "'Paintello's Paint Washable Satinée - Rich Red 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTn.jpg",
         title: "'Paintello's Paint Washable Satinée - Sweet Lavender 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTl.jpg",
         title: "'Paintello's Paint Washable Satinée - Lime Crush 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTQ.jpg",
         title: "'Paintello's Paint Washable Satinée - Waterfall 4KG",
-        price: 2490.00
+        price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTW.png",
         title: "'Paintello's Paint Washable Satinée - New Duck Egg 4KG",
-        price: 2490.00
+        price: 2590.00
     }
  
     
