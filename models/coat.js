93d6a978ff56e6c17a549be09b7140fda8d51f7b
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-03-30 15:18:52 +0100

    Update and rename coffee.js to coat.js

--- models/coat.js
+++ models/coat.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
-var coffeeSchema = new mongoose.Schema({
+var coatSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
-module.exports = mongoose.model("coffee", coffeeSchema);
+module.exports = mongoose.model("coat", coatSchema);
