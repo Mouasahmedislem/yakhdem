c5a858e608ac99b47760883302c66d9e42fccbcf
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-03-30 16:09:49 +0100

    Update saleH.js

--- seed/saleH.js
+++ seed/saleH.js
@@ -30,10 +30,12 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
     title: "Cuisine Paint",
     href:"/sale/rug"
 },
+   {
     image: "https://s7.gifyu.com/images/IMG-20220413-WA003403cc89faebbf9981.jpg",
     title: "Bathroom Paint",
     href:"/sale/cuisin"
 },
+   {
  image: "https://s7.gifyu.com/images/IMG-20220413-WA003403cc89faebbf9981.jpg",
     title: "Onecoat Paint",
     href:"/sale/coat"
