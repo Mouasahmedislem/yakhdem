d3aefa3c5af3e9e57f44e98552034c4c31c603e3
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-04-01 01:08:58 +0200

    Update rug.js

--- seed/rug.js
+++ seed/rug.js
@@ -12,11 +12,68 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
     })
 
 var rugs = [
+  {
+        image: "https://s9.gifyu.com/images/SV82T.png",
+        title: "Paintello's Paint Onecoat BEIG 02 4KG",
+        price: 2390.00
+    },
+   
     {
-        image: "https://s7.gifyu.com/images/Picsart_22-04-09_00-24-14-466.jpeg.jpg",
-        title: "Comming Soon",
-        price: 0
+        image: "https://s12.gifyu.com/images/SV82N.png",
+        title: "Paintello's Paint Onecoat Blue 09 Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82K.png",
+        title: "Paintello's Paint Onecoat Green 08 Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82H.png",
+        title: "Paintello's Paint Onecoat GREEN 18 MATT 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82x.png",
+        title: "Paintello's Paint Onecoat Grey 01 Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82w.png",
+        title: "Paintello's Paint Onecoat Pink 09 Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV829.png",
+        title: "Paintello's Paint Onecoat Pink 13 Nashville House Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82E.png",
+        title: "Paintello's Paint Onecoat  Red 01 Matt  4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82h.png",
+        title: "Paintello's Paint Onecoat Taupe 01 Matt 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s5.gifyu.com/images/SRFTl.jpg",
+        title: "Paintello's Paint Onecoat 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s12.gifyu.com/images/SV82k.png",
+        title: "Paintello's Paint Onecoat White 01 4KG",
+        price: 2590.00
+    },
+    {
+        image: "https://s9.gifyu.com/images/SV82v.png",
+        title: "Paintello's Paint Onecoat Yellow 06 Matt 4KG",
+        price: 2590.00
     }
+ 
  
 ];
 
