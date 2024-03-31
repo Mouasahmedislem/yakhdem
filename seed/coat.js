8c158c79a5577eaaa91ed112407774d1aef316f7
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-03-31 17:09:04 +0200

    Update coat.js

--- seed/coat.js
+++ seed/coat.js
@@ -13,64 +13,64 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
 
 var coats = [
      {
-        image: "https://s7.gifyu.com/images/IMG-20220418-WA0017.jpg",
-        title: "'Paintello's Paint Washable Satinée - Brilliant White 4KG",
+        image: "https://s9.gifyu.com/images/SV82T.png",
+        title: "Paintello's Paint Onecoat BEIG 02 4KG",
         price: 2390.00
     },
    
     {
-        image: "https://s5.gifyu.com/images/SRFTR.jpg",
-        title: "'Paintello's Paint Washable Satinée - Red Spice 4KG",
+        image: "https://s12.gifyu.com/images/SV82N.png",
+        title: "Paintello's Paint Onecoat Blue 09 Matt 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFT8.jpg",
-        title: "'Paintello's Paint Washable Satinée - Mocha 4KG",
+        image: "https://s12.gifyu.com/images/SV82K.png",
+        title: "Paintello's Paint Onecoat Green 08 Matt 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFT6.jpg",
-        title: "'Paintello's Paint Washable Satinée - Deep Amethyst 4KG",
+        image: "https://s12.gifyu.com/images/SV82H.png",
+        title: "Paintello's Paint Onecoat GREEN 18 MATT 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFT4.jpg",
-        title: "'Paintello's Paint Washable Satinée - Dark Angel 4KG",
+        image: "https://s12.gifyu.com/images/SV82x.png",
+        title: "Paintello's Paint Onecoat Grey 01 Matt 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTg.jpg",
-        title: "'Paintello's Paint Washable Satinée - Burnt Sugar 4KG",
+        image: "https://s12.gifyu.com/images/SV82w.png",
+        title: "Paintello's Paint Onecoat Pink 09 Matt 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTc.jpg",
-        title: "'Paintello's Paint Washable Satinée - Aqua 4KG",
+        image: "https://s12.gifyu.com/images/SV829.png",
+        title: "Paintello's Paint Onecoat Pink 13 Nashville House Matt 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTu.jpg",
-        title: "'Paintello's Paint Washable Satinée - Rich Red 4KG",
+        image: "https://s12.gifyu.com/images/SV82E.png",
+        title: "Paintello's Paint Onecoat  Red 01 Matt  4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTn.jpg",
-        title: "'Paintello's Paint Washable Satinée - Sweet Lavender 4KG",
+        image: "https://s12.gifyu.com/images/SV82h.png",
+        title: "Paintello's Paint Onecoat Taupe 01 Matt 4KG",
         price: 2590.00
     },
     {
         image: "https://s5.gifyu.com/images/SRFTl.jpg",
-        title: "'Paintello's Paint Washable Satinée - Lime Crush 4KG",
+        title: "Paintello's Paint Onecoat 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTQ.jpg",
-        title: "'Paintello's Paint Washable Satinée - Waterfall 4KG",
+        image: "https://s12.gifyu.com/images/SV82k.png",
+        title: "Paintello's Paint Onecoat White 01 4KG",
         price: 2590.00
     },
     {
-        image: "https://s5.gifyu.com/images/SRFTW.png",
-        title: "'Paintello's Paint Washable Satinée - New Duck Egg 4KG",
+        image: "https://s9.gifyu.com/images/SV82v.png",
+        title: "Paintello's Paint Onecoat Yellow 06 Matt 4KG",
         price: 2590.00
     }
  
