08e8d2d49c2dbde4a1d68480ae2aef73a1e5f81b
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-04-01 21:03:44 +0200

    Update rug.js

--- models/rug.js
+++ models/rug.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var rugSchema = new mongoose.Schema({
     image: {type: String, required: true},
+    href: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
 });
