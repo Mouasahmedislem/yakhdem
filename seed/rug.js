2850e3d5c2e854faacf0776bafcac35527cf835b
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2024-04-02 02:02:22 +0200

    Update rug.js

--- seed/rug.js
+++ seed/rug.js
@@ -15,63 +15,63 @@ var rugs = [
   {
         image: "https://s9.gifyu.com/images/SV82T.png",
         title: "Paintello's Paint Onecoat BEIG 02 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/beig02",
         price: 2390.00
     },
    
     {
         image: "https://s12.gifyu.com/images/SV82N.png",
         title: "Paintello's Paint Onecoat Blue 09 Matt 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/bleu09",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82K.png",
-        href : "/sale/yallaw",
+        href : "/onecoat/green08",
         title: "Paintello's Paint Onecoat Green 08 Matt 4KG",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82H.png",
         title: "Paintello's Paint Onecoat GREEN 18 MATT 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/green18",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82x.png",
         title: "Paintello's Paint Onecoat Grey 01 Matt 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/grey01",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82w.png",
         title: "Paintello's Paint Onecoat Pink 09 Matt 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/pink09",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV829.png",
         title: "Paintello's Paint Onecoat Pink 13 Nashville House Matt 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/pink13",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82E.png",
         title: "Paintello's Paint Onecoat  Red 01 Matt  4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/red01",
         price: 2590.00
     },
     {
         image: "https://s12.gifyu.com/images/SV82h.png",
         title: "Paintello's Paint Onecoat Taupe 01 Matt 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/taupe",
         price: 2590.00
     },
    
     {
         image: "https://s12.gifyu.com/images/SV82k.png",
         title: "Paintello's Paint Onecoat White 01 4KG",
-        href : "/sale/yallaw",
+        href : "/onecoat/white01",
         price: 2590.00
     },
     {
