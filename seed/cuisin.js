6010944a43f0886013c2b610e7261af89b8c6acd
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-01-13 21:10:34 +0100

    Update cuisin.js

--- seed/cuisin.js
+++ seed/cuisin.js
@@ -15,7 +15,7 @@ var cuisins = [
     {
         image: "https://s12.gifyu.com/images/SYnoR.jpg",
         title: "My Fixature By Paintello's Paint ",
-        href : "/onecoat/beig02",
+        href : "/myfixateur/fixateur",
         price: 1500
     }
   
