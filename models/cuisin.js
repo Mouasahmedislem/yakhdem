b4f33615ac0f28f7a223464a93479e8896a105ce
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-01-13 20:58:06 +0100

    Update cuisin.js
    add herf

--- models/cuisin.js
+++ models/cuisin.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var cuisinSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
+    href: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
