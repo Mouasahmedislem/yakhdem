7f0bad569ee2df9bbc2dd1549bbbd502461cacc1
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-01-31 17:04:32 +0100

    Update sample.js

--- seed/sample.js
+++ seed/sample.js
@@ -1,1 +1,37 @@
+var mongoose  = require('mongoose')
+var sample = require('../models/sample')
 
+
+mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.net/paintello?retryWrites=true&w=majority' , (err)=> {
+    if (err) {
+        console.log(err)
+    } else{
+        console.log('connected to db succesfuly...')
+    }
+    
+    })
+
+var samples = [
+    {
+        image: "https://s12.gifyu.com/images/SYnoR.jpg",
+        title: "My Fixature By Paintello's Paint ",
+        href : "/myfixateur/fixateur",
+        price: 1500
+    }
+  
+ 
+];
+
+function seedsample(){
+    sample.remove({}, err => {
+        if(err) console.log(err);
+        samples.forEach(seed => {
+            sample.create(seed, (err, sample) => {
+                if(err) console.log(err);
+                sample.save();
+            });
+        });
+    });
+}
+
+module.exports = seedsample;
