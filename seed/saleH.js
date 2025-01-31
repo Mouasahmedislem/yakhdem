d137d8936d7c7f98cfbf27e82f87022a36c7ea45
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-01-31 20:41:38 +0100

    Update saleH.js

--- seed/saleH.js
+++ seed/saleH.js
@@ -40,6 +40,11 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
     title: "Samples",
     href:"/sale/sample"
 },
+    {
+    image: "https://s13.gifyu.com/images/b2pmM.jpg",
+    title: "Paintello's MAC",
+    href:"/sale/coat"
+},
  
 {
     image: "https://cdn.bmstores.co.uk/images/dmCategory/thumbnailImageResponsive/decorating-320x3201.jpg",
