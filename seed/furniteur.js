ab646158f8662e71024709cbf47b0b4c7f0e15be
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-03-07 23:45:09 +0100

    Update furniteur.js

--- seed/furniteur.js
+++ seed/furniteur.js
@@ -13,61 +13,127 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
 
 var furniteurs = [
     {
-        image: "https://s12.gifyu.com/images/SVZ7a.png",
+        image: "https://s3.gifyu.com/images/bbQZf.png",
         title: "Paintello Paint Vinyl Silk Emulsion - Brilliant White 4KG",
         price: 900.00
     },
    
     {
-        image: "https://s12.gifyu.com/images/SVZ7V.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Red Spice 4KG",
+        image: "https://s3.gifyu.com/images/bbQa6.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - Brilliant White  10KG",
         price: 1100.00
     },
     {
-        image: "https://s9.gifyu.com/images/SVZ7f.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Mocha 4KG",
+        image: "https://s3.gifyu.com/images/bbQZl.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - blue09 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ7Z.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Deep Amethyst 4KG",
+        image: "https://s3.gifyu.com/images/bbQZn.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - blue08 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ7U.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Dark Angel 4KG",
+        image: "https://s3.gifyu.com/images/bbQZu.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - beige09 4KG",
         price: 1100.00
     },
     {
-        image: "https://s9.gifyu.com/images/SVZ7F.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Burnt Sugar 4KG",
+        image: "https://s3.gifyu.com/images/bbQZQ.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - beige02 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ7R.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Aqua 4KG",
+        image: "https://s3.gifyu.com/images/bbQZW.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - beige01 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ7i.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Rich Red 4KG",
+        image: "https://s3.gifyu.com/images/bbQZc.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - blue14 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ7j.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Sweet Lavender 4KG",
+        image: "https://s3.gifyu.com/images/bbQZg.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - blue15 4KG",
         price: 1100.00
     },
     {
-        image: "https://s12.gifyu.com/images/SVZ70.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Lime Crush 4KG",
+        image: "https://s3.gifyu.com/images/bbQZ4.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - green08 4KG",
         price: 1100.00
     },
-  {
-        image: "https://s12.gifyu.com/images/SVZ7C.png",
-        title: "Paintello Paint Vinyl Silk Emulsion - Beig  4KG",
+    {
+        image: "https://s3.gifyu.com/images/bbQZ6.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - green09 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZ8.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - green13 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZR.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - grey01 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZi.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - grey03 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZj.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - grey14 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZ0.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - pink02  4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZC.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - pink03 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZF.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - pink07 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZU.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - pink09 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZV.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - pink13 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZZ.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - taupe01 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZY.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - yellow01 4KG",
+        price: 1100.00
+    },
+    {
+        image: "https://s3.gifyu.com/images/bbQZr.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - yellow05 4KG",
+        price: 1100.00
+    },
+     {
+        image: "https://s3.gifyu.com/images/bbQZt.png",
+        title: "Paintello Paint Vinyl Silk Emulsion - yellow06 4KG",
         price: 1100.00
     }
+     
  
 ];
 
