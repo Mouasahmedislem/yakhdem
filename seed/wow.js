40dd4e7b323ebdad601dafd4b2597a480565547f
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-04-20 10:34:18 +0200

    Update wow.js

--- seed/wow.js
+++ seed/wow.js
@@ -12,79 +12,14 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
     })
 var wows = [
     {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/platoir-decorative-inox-trapeze-768x512.jpg",
-        title: "Platoir inox ",
-        price: 350
+        image: "https://live.staticflickr.com/65535/54462517577_300e7d50b3_c.jpg",
+        title: "Paintello's Tile Paint BLACK 1KG + Mini Rouleaux offret ",
+        price: 3500
     },
     {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/Couteau-pate-enduit%E2%80%8B.jpg",
-        title: "Couteau à Enduire Lame Arrondie",
-        price: 500
-    },
-    {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/couteau-enduire-droite-768x512.jpg",
-        title: "Couteau à Enduire Lame Droite",
-        price: 920
-    },
-    {
-        image: "https:https://rolux.dz/wp-content/uploads/2022/02/ruban-adhesif-masquage-768x511.jpg",
-        title: "Papier Adhésif De Masquage",
-        price: 2350
-    },
-    {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/spalter-768x512.jpg",
-        title: "Spalter",
-        price: 3420
-    },
-    {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/brosse-peinture-plate-768x512.jpg",
-        title: "Brosse Plate",
-        price: 3750
-    },
-    {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/pinceau-plat-bleu-peinture-768x512.jpg",
-        title: "Pinceau Plat Manche Bleu",
-        price: 1550
-    },
-    {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/pinceau-rond-peinture-768x512.jpg",
-        title: "Pinceau Rond Manche Bi-Matiére",
-        price: 1550
-    },
-  {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/rouleaux-decorative-nid-dabeille-768x512.jpg",
-        title: "Rouleaux Nid D'abeille 200 mm / 80 Ø",
-        price: 1550
-    },
-  {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/recharge-mini-rouleau-peinture-eponge-768x512.jpg",
-        title: "Recharge Mini Rouleau Eponge",
-        price: 1550
-    },
-  {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/recharge-mini-rouleau-peinture-polyacrylique-768x512.jpg",
-        title: "Recharge Mini Rouleau PolyAcrylique",
-        price: 1550
-    },
-  {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/monture-mini-rouleau-peinture-768x512.jpg",
-        title: "Monture Mini Rouleau ",
-        price: 1550
-    },
-   {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/mini-rouleau-peinture-microfibre-768x512.jpg",
-        title: "Mini Rouleau Microfibre Antigoutte 120 mm / 25 Ø",
-        price: 1550
-    },
-   {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/Rouleau-peinture-acrylique-Facade-768x513.jpg",
-        title: "Rouleau Acrylique Façade 200 mm / 44 Ø",
-        price: 1550
-    },
-   {
-        image: "https://rolux.dz/wp-content/uploads/2022/02/Rouleau-peinture-microfibre--768x513.jpg",
-        title: "Rouleau Microfibre 200 mm / 44 Ø",
-        price: 1550
+        image: "https://live.staticflickr.com/65535/54463354461_df6e1384da_c.jpg",
+        title: "Paintello's Tile Paint white 1KG  + Mini Rouleaux offret",
+        price: 3500
     }
   
  
