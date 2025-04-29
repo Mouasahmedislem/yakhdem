00ab68bb9e233dcc35ec6bb2c51d78f94b8e90d8
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-04-29 18:37:20 +0200

    Update tool.js

--- models/tool.js
+++ models/tool.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var toolSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
-    href: {type: String, required: true}
+    price: {type: Number, required: true}
 });
 
 module.exports = mongoose.model("tool", toolSchema);
