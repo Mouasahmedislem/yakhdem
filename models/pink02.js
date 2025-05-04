346bfa0a3824f7b98f41a9e28d95b96ca5435ca2
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-04 23:03:35 +0200

    Update pink02.js

--- models/pink02.js
+++ models/pink02.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
-var pink02Schema = new mongoose.Schema({
+var pinksapSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
-module.exports = mongoose.model("pink02", pink02Schema);
+module.exports = mongoose.model("pinksap", pinksapSchema);
