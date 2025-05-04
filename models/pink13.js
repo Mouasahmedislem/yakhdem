29b365a454d3591f214ec0e801cf7e7450fcefbd
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-04 20:07:20 +0200

    Update pink13.js

--- models/pink13.js
+++ models/pink13.js
@@ -1,1 +1,9 @@
+var mongoose = require("mongoose");
 
+var pink13Schema = new mongoose.Schema({
+    image: {type: String, required: true},
+    title: {type: String, required: true},
+    price: {type: Number, required: true}
+});
+
+module.exports = mongoose.model("pink13", pink09Schema);
