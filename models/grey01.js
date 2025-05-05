a7141497e2d02e0709991c99b86e4a2c6f4b3568
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-05 11:23:42 +0200

    Update grey01.js

--- models/grey01.js
+++ models/grey01.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
-var grey01Schema = new mongoose.Schema({
+var greyaSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
-module.exports = mongoose.model("grey01", grey01Schema);
+module.exports = mongoose.model("greya", greyaSchema);
