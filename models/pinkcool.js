4eedd2c8660f412bef6128c12d97d3ab5ed3edf9
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-05 09:14:25 +0200

    Update and rename pink13.js to pinkcool.js

--- models/pinkcool.js
+++ models/pinkcool.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
-var pink13Schema = new mongoose.Schema({
+var pinkcoolSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
-module.exports = mongoose.model("pink13", pink09Schema);
+module.exports = mongoose.model("pinkcool", pinkcoolSchema);
