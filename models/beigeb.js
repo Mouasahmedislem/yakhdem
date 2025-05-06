33bed39fbea3bec72a4277fc17cebd007e2f4268
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-06 07:43:09 +0200

    Update beigeb.js

--- models/beigeb.js
+++ models/beigeb.js
@@ -1,4 +1,6 @@
-beigebSchema = new mongoose.Schema({
+var mongoose = require("mongoose");
+
+var beigebSchema = new mongoose.Schema({
     image: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true}
