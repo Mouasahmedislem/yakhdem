fcfa8907b096f22026ade46d52dd1b490195130f
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-06 10:48:51 +0200

    Update rug.js

--- seed/rug.js
+++ seed/rug.js
@@ -55,18 +55,7 @@ var rugs = [
         href : "/onecoat/beige01",
         price: 3300.00
     },
-    {
-        image: "https://live.staticflickr.com/65535/54484155266_29874ac2b4.jpg",
-        title: "Paintello's Paint Onecoat PINKPOSY_1072P  Matt  4KG",
-        href : "/onecoat/beige03",
-        price: 3300.00
-    },
-    {
-        image: "https://live.staticflickr.com/65535/54484435238_4b1f4097ac.jpg",
-        title: "Paintello's Paint Onecoat PETITO_984P Matt 4KG",
-        href : "/onecoat/beige04",
-        price: 3300.00
-    },
+    
    
     {
         image: "https://live.staticflickr.com/65535/54484348844_c0c950df9c.jpg",
@@ -84,24 +73,6 @@ var rugs = [
         image: "https://live.staticflickr.com/65535/54484155326_4b1627c169.jpg",
         title: "Paintello's Paint Onecoat LITTELSTAR_1184P Matt 4KG",
         href : "/onecoat/beige07",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484155351_e3fbf16511.jpg",
-        title: "Paintello's Paint Onecoat LITTELFALS_1192P Matt 4KG",
-        href : "/onecoat/beige08",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484155361_ce56a9febd.jpg",
-        title: "Paintello's Paint Matt LIGHTE_1182P Onecoat 4KG",
-        href : "/onecoat/beige20",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484155376_5456429744.jpg",
-        title: "Paintello's Paint Onecoat IVORY_1064P Matt 4KG",
-        href : "/onecoat/beige10",
         price: 3300.00
     },
   {
@@ -128,36 +99,7 @@ var rugs = [
         href : "/onecoat/beige14",
         price: 3300.00
     },
-  {
-        image: "https://live.staticflickr.com/65535/54483309512_01f24c34f5.jpg",
-        title: "Paintello's Paint Onecoat CLOVER_982P Matt 4KG",
-        href : "/onecoat/beige15",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484435448_41023c447c.jpg",
-        title: "Paintello's Paint Onecoat BEIGE01_978P Matt 4KG",
-        href : "/onecoat/beige16",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54483309542_78d258e714.jpg",
-        title: "Paintello's Paint Onecoat AMRIAL_980P Matt 4KG",
-        href : "/onecoat/beige17",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484512515_ff20889f4b.jpg",
-        title: "Paintello's Paint Onecoat ZINNIA_1078P Matt 4KG",
-        href : "/onecoat/beige18",
-        price: 3300.00
-    },
-  {
-        image: "https://live.staticflickr.com/65535/54484155506_d4ef44d858.jpg",
-        title: "Paintello's Paint Onecoat SANDLIGHT_972P Matt 4KG",
-        href : "/onecoat/beige19",
-        price: 3300.00
-    },
+  
   {
         image: "https://live.staticflickr.com/65535/54483123844_144752d2a2.jpg",
         title: "Paintello's Paint Onecoat LITTELYELLOW_164P Matt 4KG",
@@ -194,18 +136,7 @@ var rugs = [
         href : "/onecoat/grey15",
         price: 3300.00
     },
-   {
-        image: "https://live.staticflickr.com/65535/54482441799_7815311a50.jpg",
-        title: "Paintello's Paint Onecoat DROPGREY_158P Matt 4KG",
-        href : "/onecoat/grey16",
-        price: 3300.00
-    },
-   {
-        image: "https://live.staticflickr.com/65535/54482525483_4fed617ea7.jpg",
-        title: "Paintello's Paint Onecoat BRIGHT_1196P Matt 4KG",
-        href : "/onecoat/grey17",
-        price: 3300.00
-    },
+  
    {
         image: "https://live.staticflickr.com/65535/54481290707_f73e099c43.jpg",
         title: "Paintello's Paint Onecoat FRESHOGREEN_758P Matt 4KG",
@@ -236,24 +167,7 @@ var rugs = [
         href : "/onecoat/green19",
         price: 3300.00
     },
-   {
-        image: "https://live.staticflickr.com/65535/54482335509_01471e2b62.jpg",
-        title: "Paintello's Paint Onecoat PLUMEGREEN_162P Matt 4KG",
-        href : "/onecoat/green20",
-        price: 3300.00
-    },
-   {
-        image: "https://live.staticflickr.com/65535/54482417943_8c2b32a7b3.jpg",
-        title: "Paintello's Paint Onecoat LITTELGREEN_182P Matt 4KG",
-        href : "/onecoat/green21",
-        price: 3300.00
-    },
-   {
-        image: "https://live.staticflickr.com/65535/54482500350_19c6cfc28d.jpg",
-        title: "Paintello's Paint Onecoat GARDEN_1198P Matt 4KG",
-        href : "/onecoat/green22",
-        price: 3300.00
-    },
+   
    {
         image: "https://live.staticflickr.com/65535/54481135987_b23529b174.jpg",
         title: "Paintello's Paint Onecoat BLUEDROP_756P Matt 4KG",
@@ -265,19 +179,8 @@ var rugs = [
         title: "Paintello's Paint Onecoat COOLMORA_744P Matt 4KG",
         href : "/onecoat/bleue14",
         price: 3300.00
-    },
-   {
-        image: "https://live.staticflickr.com/65535/54482263638_167c17c6e5.jpg",
-        title: "Paintello's Paint Onecoat FRESHOBLUE_742P Matt 4KG",
-        href : "/onecoat/blue08",
-        price: 3300.00
-    },
-   {
-        image: "https://live.staticflickr.com/65535/54481135862_0261d66c2a.jpg",
-        title: "Paintello's Paint Onecoat TWEEDIA_752P Matt 4KG",
-        href : "/onecoat/blue15",
-        price: 3300.00
-    },
+    }
+
      
  
  
