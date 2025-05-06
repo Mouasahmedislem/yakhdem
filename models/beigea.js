f7f01faa2326511b1e180c2b5a7ac1d4abc161dc
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-06 07:41:11 +0200

    Update beigea.js

--- models/beigea.js
+++ models/beigea.js
@@ -1,1 +1,7 @@
+beigeaSchema = new mongoose.Schema({
+    image: {type: String, required: true},
+    title: {type: String, required: true},
+    price: {type: Number, required: true}
+});
 
+module.exports = mongoose.model("beige", beigeaSchema);
