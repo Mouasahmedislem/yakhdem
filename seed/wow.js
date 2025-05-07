5930da5c170fef9389f243e545745fa09e3c5ee2
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-07 19:29:24 +0200

    Update wow.js

--- seed/wow.js
+++ seed/wow.js
@@ -13,12 +13,12 @@ mongoose.connect('mongodb+srv://Islem:cmygNChSy2L9Q4xt@paintello.cu30n.mongodb.n
 var wows = [
     {
         image: "https://live.staticflickr.com/65535/54484923974_c91b3fedd5.jpg",
-        title: "Paintello's Tile Paint BLACK 1.5KG + Mini Rouleaux offret ",
+        title: "Paintello's Tile Paint BLACK 1KG + Mini Rouleaux offret ",
         price: 3650
     },
     {
         image: "https://live.staticflickr.com/65535/54485082775_b762fc787d.jpg",
-        title: "Paintello's Tile Paint white 1.5KG  + Mini Rouleaux offret",
+        title: "Paintello's Tile Paint white 1KG  + Mini Rouleaux offret",
         price: 3500
     }
 ];
