489fcf6264bfb72a053785bda11aebcf28dd2cd3
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-05-11 22:03:47 +0200

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,8 @@ module.exports = function Cart(oldCart){
     this.items = oldCart.items || {};
     this.totalQty = oldCart.totalQty || 0;
     this.totalPrice = oldCart.totalPrice || 0;
-   
+    this.shippingPrice = shippingPrices[wilaya] || 0;
+  
     this.add = function(item, id){
         var storedItem = this.items[id];
         if(!storedItem){
