0a803b5236ab7eaf65a461aa1e36985eed64591a
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-06-14 17:23:31 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
-
+var mongoose = require('mongoose');
 var express = require('express');
 var session = require('express-session');
 var passport = require('passport');
