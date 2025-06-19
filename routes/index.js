f8fc9918f98fad29f0e92f3aaa52019fd93cb575
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-06-19 03:05:41 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ var Paintello = require('../models/paintello');
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const Order = require('../models/order');
 const Powers = require('../models/powers');
-const middleware = require('../middleware');
+const middleware = require('../middleware/index');
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 
