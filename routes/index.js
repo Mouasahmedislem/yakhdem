1b35930f58208f3e98f8662b4cc5d975b0bddfaf
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-06-24 10:23:09 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2082,7 +2082,7 @@ router.get('/track-order', async (req, res) => {
 
 router.get("/producthome/:id", async (req, res) => {
   const producthome = await Producthome.findById(req.params.id);
-  const eventId = req.body.eventId ;
+  const eventId = `view_${producthome.id}_${Date.now()}`;
 
   // ✅ Use req.user directly — no fallback needed
   console.log("✅ req.user", req.user); // debug
