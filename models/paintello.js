6314cc2157535cd9f2aabdf8c364959938639381
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-07-01 22:26:18 +0200

    Update paintello.js

--- models/paintello.js
+++ models/paintello.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const paintelloSchema = new mongoose.Schema({
   title: String,
   price: Number,
-  image: [String]
+  image: [String],
+  href: String
 });
 
 module.exports = mongoose.model('Paintello', paintelloSchema);
