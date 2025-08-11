4d4465df9f0853440020cbf4b84af54916168ff5
Mouasahmedislem <100952420+Mouasahmedislem@users.noreply.github.com>
2025-08-11 14:56:01 +0200

    Update producthome.js

--- models/producthome.js
+++ models/producthome.js
@@ -5,9 +5,13 @@ const producthomeSchema = new mongoose.Schema({
   title: String,
   price: Number,
   image: [String],         // For image slider
-  description: String,      // Description tab
-  details: Object,          // Info tab
-  type: String              // Optional: used for add-to-cart route logic
+  description: String,     // Description tab
+  details: Object,         // Info tab
+  type: String,            // Optional: used for add-to-cart route logic
+  videoUrl: {              // YouTube embed link (optional)
+    type: String,
+    required: false
+  }
 });
 
 module.exports = mongoose.model('Producthome', producthomeSchema);
